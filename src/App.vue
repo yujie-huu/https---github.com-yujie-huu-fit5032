468241bd676e59444dc0511e9d9e3fab4b6fffd0
Yujie Hu <yhuu0149@student.monash.edu>
2024-08-29 13:56:53 +1000

    router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 import JSON from './assets/json/JSON.vue'
 import Form from './views/HomeView.vue'
+import BHeader from './components/BHeader.vue'
 </script>
 
 <template>
-  <!-- <JSON /> -->
-  <Form />
+  <div class="main-container">
+    <header>
+      <BHeader />
+    </header>
+
+    <main class="main-box">
+      <!-- <LibraryRegistrationForm /> -->
+      <!-- <JSONLab /> -->
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
-// import './style.css'
+import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
+app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(router)
 
-createApp(App).mount('#app')
+app.mount('#app')
